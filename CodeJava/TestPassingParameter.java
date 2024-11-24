e9a801e411d45e09050ece71b65e1d0ae3f17355
tqdung11a3 <truongquocdung2004@gmail.com>
2024-11-24 21:40:16 +0700

    bai 3

--- CodeJava/TestPassingParameter.java
+++ CodeJava/TestPassingParameter.java
@@ -14,10 +14,10 @@ public class TestPassingParameter {
 		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
 	}
 	
-	public static void swap(Object o1, Object o2) {
-		Object tmp = o1;
-		o1 = o2;
-		o2 = tmp;
+	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		DigitalVideoDisc tmp = new DigitalVideoDisc(dvd1.getTitle());
+		dvd1.setTitle(dvd2.getTitle());
+		dvd2.setTitle(tmp.getTitle());
 	}
 	
 	public static void changeTitle(DigitalVideoDisc dvd, String title) {
